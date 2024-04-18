7cafb6820d41d503fa8be41736cb3788bbffc228
V4nz05 <142322436+V4nz05@users.noreply.github.com>
2024-04-18 15:57:30 -0300

    att

---
+++ PI3_Sim/Assets/temp.cs
@@ -0,0 +1,23 @@
+using System.Collections;
+using System.Collections.Generic;
+using TMPro;
+using UnityEngine;
+using UnityEngine.UI;
+
+public class temp : MonoBehaviour
+{
+    public TextMeshProUGUI temperatura;
+
+
+    // Start is called before the first frame update
+    void Start()
+    {
+        temperatura.text = "oi";
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        
+    }
+}
