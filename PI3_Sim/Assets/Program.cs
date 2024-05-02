96ab615251afe12e80d18b83a5eb13ce98a71a3e
luizns2003 <165841719+luizns2003@users.noreply.github.com>
2024-05-02 14:38:46 -0300

    Corrigido bug da biblioteca json

--- PI3_Sim/Assets/Program.cs
+++ PI3_Sim/Assets/Program.cs
@@ -1,8 +1,4 @@
-﻿using System;
-using System.Collections.Generic;
-using System.Net.Http;
-using System.Threading.Tasks;
-using Newtonsoft.Json;
+﻿using System.Threading.Tasks;
 using UnityEngine;
 using PI3_Sim.Classes;
 
